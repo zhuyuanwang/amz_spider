a91bc3a534d0b97500aee647759d166ff5f85b41
zhuyuanwang <17746572170@163.com>
2022-09-30 23:35:48 +0800

    第一次提交代码

---
+++ spiders/amz_review_spider.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/9/30 23:29
+# @Author  : Circleone_
+
+import json
+import re
+import sys
+import time
+import redis
+import requests
+import traceback
+import threading
+
+class MainSpider(threading.Thread):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
